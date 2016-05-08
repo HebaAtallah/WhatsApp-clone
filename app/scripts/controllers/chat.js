7d006ba8c6b283f3f01d917a2a6825e2b558fce4
Heba <heba@imidos.com>
2016-05-08 16:06:38 +0200

    chat send text with message time

--- app/scripts/controllers/chat.js
+++ app/scripts/controllers/chat.js
@@ -8,10 +8,53 @@
  * Controller of the whatsAppCloneApp
  */
 angular.module('whatsAppCloneApp')
-  .controller('ChatCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('ChatCtrl', function ($scope, $http) {
+
+    // get messages
+    $scope.chatData=[];
+      $http({
+        method: 'GET',
+        url: 'http://localhost:3000/data2/2'
+      }).then(function successCallback(response) {
+
+          $scope.chatData=response.data;
+          console.log($scope.chatData);
+
+        }, function errorCallback(response) {
+
+          console.log("error");
+        });
+
+
+        // sendMessage
+      $scope.sendMessage = function(){
+
+
+        new Date($.now());
+        var dt = new Date();
+        var getTime = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+
+
+    		var data = {
+    				messages :$scope.chatData.messages
+
+        };
+
+        data.messages.push({details:$scope.message,'time':getTime,'type':'message-out'});
+
+      var url = 'http://localhost:3000/data2/2';
+        $http.patch(url, data).then(function (response) {
+          console.log(response.data);
+
+          $scope.message='';
+
+            console.log('all is good', response.data);
+        }, function (error) {
+
+            console.log('an error occurred', error.data);
+        });
+
+
+
+      };
+});
