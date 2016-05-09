186aa910670272ac613d48be31bf96d4a4add9df
Heba <heba@imidos.com>
2016-05-09 15:42:26 +0200

    form validation

--- app/scripts/controllers/chat.js
+++ app/scripts/controllers/chat.js
@@ -30,29 +30,162 @@ var app=angular.module('whatsAppCloneApp')
       // sendMessage
       $scope.sendMessage = function(){
 
+        // data object
+        var data = {
+              messages :$scope.chatData.messages
+
+        };
+
+        var image=$scope.image;
+        var messageDetails=$scope.message;
+
+        // get date
         new Date($.now());
         var dt = new Date();
         var times = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
 
-    		var data = {
-    				messages :$scope.chatData.messages
 
+        // uploading functions
+        var uploadOnlyImage = function() {
+          data.messages.push({'time':times,'type':'message-out',image:'images/'+image});
+          var url = 'http://localhost:3000/data2/2';
+            $http.patch(url, data).then(function (response) {
+              console.log(response.data);
+
+              $scope.message='';
+
+                console.log('all is good', response.data);
+            }, function (error) {
+
+                console.log('an error occurred', error.data);
+            });
+        };
+
+        var uploadOnlyText = function() {
+          data.messages.push({details:messageDetails,'time':times,'type':'message-out'});
+          var url = 'http://localhost:3000/data2/2';
+            $http.patch(url, data).then(function (response) {
+              $scope.message='';
+
+                console.log('all is good', response.data);
+            }, function (error) {
+
+                console.log('an error occurred', error.data);
+            });
+        };
+
+        var uploadTextAndImage = function() {
+          data.messages.push({details:messageDetails,'time':times,'type':'message-out',image:'images/'+image});
+          var url = 'http://localhost:3000/data2/2';
+            $http.patch(url, data).then(function (response) {
+              console.log(response.data);
+
+              $scope.message='';
+
+                console.log('all is good', response.data);
+            }, function (error) {
+
+                console.log('an error occurred', error.data);
+            });
         };
 
 
-      data.messages.push({details:$scope.message,'time':times,'type':'message-out',image:'images/'+$scope.image});
+        // empty message
+        if(messageDetails==undefined && image==undefined ){
+          $scope.alertMessage=true;
+          $scope.alertMessageContent="The message is empty";
+        }
 
-      var url = 'http://localhost:3000/data2/2';
-        $http.patch(url, data).then(function (response) {
-          console.log(response.data);
+        if(messageDetails=='' && image==undefined){
+          messageDetails=undefined;
+          $scope.alertMessage=true;
+          $scope.alertMessageContent="The message is empty";
+        }
 
-          $scope.message='';
+        if(messageDetails=='' && image !=undefined){
+          messageDetails=undefined;
 
-            console.log('all is good', response.data);
-        }, function (error) {
+          var getImageExtension= image.substring(image.lastIndexOf('.') + 1).toLowerCase();
+          var imageRightExtension=false;
 
-            console.log('an error occurred', error.data);
-        });
+          if (getImageExtension  == "gif" || getImageExtension == "png" || getImageExtension == "bmp"
+                  || getImageExtension == "jpeg" || getImageExtension == "jpg") {
 
-      };
+                imageRightExtension=true;
+          }
+          // right extension
+          if (imageRightExtension==true) {
+
+            $scope.alertMessage=false;
+            uploadOnlyImage();
+
+          }
+          // wrong extension
+          else if (imageRightExtension==false) {
+            $scope.alertMessage=true;
+            $scope.alertMessageContent="You should upload only images";
+          }
+        }
+
+        // only text
+        if (messageDetails !=undefined && image ==undefined){
+              $scope.alertMessage=false;
+              uploadOnlyText();
+          }
+
+        // only image and image validation
+        if (messageDetails==undefined && image !=undefined) {
+
+          var getImageExtension= image.substring(image.lastIndexOf('.') + 1).toLowerCase();
+          var imageRightExtension=false;
+
+          if (getImageExtension  == "gif" || getImageExtension == "png" || getImageExtension == "bmp"
+                  || getImageExtension == "jpeg" || getImageExtension == "jpg") {
+
+                imageRightExtension=true;
+          }
+
+          // right extension
+          if (imageRightExtension==true) {
+
+            $scope.alertMessage=false;
+            uploadOnlyImage();
+
+          }
+          // wrong extension
+          else if (imageRightExtension==false) {
+            $scope.alertMessage=true;
+            $scope.alertMessageContent="You should upload only images";
+          }
+        }
+
+        // text and image
+      if (messageDetails!=undefined && image !=undefined) {
+
+        var getImageExtension= image.substring(image.lastIndexOf('.') + 1).toLowerCase();
+        var imageRightExtension=false;
+
+        if (getImageExtension  == "gif" || getImageExtension == "png" || getImageExtension == "bmp"
+                || getImageExtension == "jpeg" || getImageExtension == "jpg") {
+
+                  imageRightExtension=true;
+        }
+        console.log(imageRightExtension);
+        // right extension
+        if (imageRightExtension==true) {
+
+          $scope.alertMessage=false;
+
+          uploadTextAndImage();
+        }
+        // wrong extension
+        else if (imageRightExtension==false) {
+          $scope.alertMessage=true;
+          $scope.alertMessageContent="You should upload only images";
+          uploadOnlyText();
+
+        }
+      }
+
+    };
 });
